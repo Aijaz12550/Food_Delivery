103e8b760fd9c8f1b5a46ffaf85fe2e4d183b4de
Aijaz12550 <42912599+Aijaz12550@users.noreply.github.com>
2019-06-28 11:22:09 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase'
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyBHcTkGUmNUjQvEASaXasvWUiE2dNGAPdg",
+    apiKey: "***********************",
     authDomain: "food-delivery23.firebaseapp.com",
     databaseURL: "https://food-delivery23.firebaseio.com",
     projectId: "food-delivery23",
-    storageBucket: "food-delivery23.appspot.com",
-    messagingSenderId: "443235899488",
-    appId: "1:443235899488:web:a4a015aaf500e04b"
+    storageBucket: "**********************",
+    messagingSenderId: '***********',
+    appId: "****************************"
 
   };
 
